eff323ad8cf26b2b3c640c2b1137edbd97b8e8d3
Andre Heider <a.heider@gmail.com>
2025-02-11 19:27:19 +0100

    wip: fan component

---
+++ components/miot/fan/__init__.py
@@ -0,0 +1,104 @@
+from esphome.components import fan
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.const import (
+    CONF_DIRECTION,
+    CONF_MAX_VALUE,
+    CONF_MIN_VALUE,
+    CONF_OPTIONS,
+    CONF_OSCILLATING,
+    CONF_OUTPUT_ID,
+    CONF_PRESET_MODES,
+    CONF_SPEED,
+    CONF_STATE,
+    CONF_STEP,
+)
+from .. import (
+    miot_ns,
+    CONF_MIOT_ID,
+    CONF_MIOT_SIID,
+    CONF_MIOT_PIID,
+    Miot,
+)
+
+DEPENDENCIES = ["miot"]
+
+MiotFan = miot_ns.class_("MiotFan", cg.Component, fan.Fan)
+
+def ensure_option_map(value):
+    cv.check_not_templatable(value)
+    option = cv.All(cv.int_range(0, 2**32 - 1))
+    mapping = cv.All(cv.string_strict)
+    options_map_schema = cv.Schema({option: mapping})
+    value = options_map_schema(value)
+
+    all_values = list(value.keys())
+    unique_values = set(value.keys())
+    if len(all_values) != len(unique_values):
+        raise cv.Invalid("Mapping values must be unique.")
+
+    return value
+
+CONFIG_SCHEMA = cv.All(
+    fan.FAN_SCHEMA.extend(
+        {
+            cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(MiotFan),
+            cv.GenerateID(CONF_MIOT_ID): cv.use_id(Miot),
+            cv.Required(CONF_STATE): cv.Schema(
+                {
+                    cv.Required(CONF_MIOT_SIID): cv.uint32_t,
+                    cv.Required(CONF_MIOT_PIID): cv.uint32_t,
+                }
+            ),
+            cv.Required(CONF_SPEED): cv.Schema(
+                {
+                    cv.Required(CONF_MIOT_SIID): cv.uint32_t,
+                    cv.Required(CONF_MIOT_PIID): cv.uint32_t,
+                    cv.Required(CONF_MIN_VALUE): cv.uint32_t,
+                    cv.Required(CONF_MAX_VALUE): cv.uint32_t,
+                    cv.Optional(CONF_STEP, default=1): cv.uint32_t,
+                }
+            ),
+            cv.Optional(CONF_OSCILLATING): cv.Schema(
+                {
+                    cv.Required(CONF_MIOT_SIID): cv.uint32_t,
+                    cv.Required(CONF_MIOT_PIID): cv.uint32_t,
+                }
+            ),
+            cv.Optional(CONF_DIRECTION): cv.Schema(
+                {
+                    cv.Required(CONF_MIOT_SIID): cv.uint32_t,
+                    cv.Required(CONF_MIOT_PIID): cv.uint32_t,
+                }
+            ),
+            cv.Optional(CONF_PRESET_MODES): cv.Schema(
+                {
+                    cv.Required(CONF_MIOT_SIID): cv.uint32_t,
+                    cv.Required(CONF_MIOT_PIID): cv.uint32_t,
+                    cv.Required(CONF_OPTIONS): ensure_option_map,
+                }
+            ),
+        }
+    ).extend(cv.COMPONENT_SCHEMA),
+)
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_MIOT_ID])
+
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID], parent)
+    await cg.register_component(var, config)
+    await fan.register_fan(var, config)
+
+    state = config.get(CONF_STATE)
+    cg.add(var.set_state_config(state[CONF_MIOT_SIID], state[CONF_MIOT_PIID]))
+    speed = config.get(CONF_SPEED)
+    cg.add(var.set_speed_config(speed[CONF_MIOT_SIID], speed[CONF_MIOT_PIID], speed[CONF_MIN_VALUE], speed[CONF_MAX_VALUE], speed[CONF_STEP]))
+    if oscillating := config.get(CONF_OSCILLATING):
+        cg.add(var.set_oscillating_config(oscillating[CONF_MIOT_SIID], oscillating[CONF_MIOT_PIID]))
+    if direction := config.get(CONF_DIRECTION):
+        cg.add(var.set_direction_config(direction[CONF_MIOT_SIID], direction[CONF_MIOT_PIID]))
+    if preset_modes := config.get(CONF_PRESET_MODES):
+        cg.add(var.set_preset_modes_config(preset_modes[CONF_MIOT_SIID], preset_modes[CONF_MIOT_PIID]))
+        options_map = preset_modes[CONF_OPTIONS]
+        for k in options_map:
+            cg.add(var.set_preset_mode(k, options_map[k]))
---
+++ components/miot/fan/miot_fan.cpp
@@ -0,0 +1,113 @@
+#include "esphome/core/log.h"
+#include "miot_fan.h"
+
+namespace esphome {
+namespace miot {
+
+static const char *const TAG = "miot.fan";
+
+// all properties are always polled
+// "state" and "oscillating" are true booleans ("true" and "false" on the wire)
+// "direction" is a true boolean as well, where "true" means REVERSE
+// what about "restore_mode"?
+
+void MiotFan::setup() {
+  this->parent_->register_listener(this->state_siid_, this->state_piid_, true, mvtString, [this](const MiotValue &value) {
+    this->state = value.as_string == "true";
+    ESP_LOGV(TAG, "MCU reported state %" PRIu32 ":%" PRIu32 " is: %s", this->state_siid_, this->state_piid_, ONOFF(this->state));
+    this->publish_state();
+  });
+  this->parent_->register_listener(this->speed_siid_, this->speed_piid_, true, mvtUInt, [this](const MiotValue &value) {
+    ESP_LOGV(TAG, "MCU reported speed %" PRIu32 ":%" PRIu32 " is: %u", this->speed_siid_, this->speed_piid_, value.as_uint);
+    this->speed = value.as_uint / this->speed_step_ - this->speed_min_ + 1;
+    this->publish_state();
+  });
+  if (this->oscillating_siid_ != 0 && this->oscillating_piid_ != 0) {
+    this->parent_->register_listener(this->oscillating_siid_, this->oscillating_piid_, true, mvtString, [this](const MiotValue &value) {
+      this->oscillating = value.as_string == "true";
+      ESP_LOGV(TAG, "MCU reported oscillating %" PRIu32 ":%" PRIu32 " is: %u", this->oscillating_siid_, this->oscillating_piid_, ONOFF(this->oscillating));
+      this->publish_state();
+    });
+  }
+  if (this->direction_siid_ != 0 && this->direction_piid_ != 0) {
+    this->parent_->register_listener(this->direction_siid_, this->direction_piid_, true, mvtString, [this](const MiotValue &value) {
+      this->direction = value.as_string == "true" ? fan::FanDirection::REVERSE : fan::FanDirection::FORWARD;
+      ESP_LOGV(TAG, "MCU reported reverse direction %" PRIu32 ":%" PRIu32 " is: %u", this->direction_siid_, this->direction_piid_, ONOFF(this->direction));
+      this->publish_state();
+    });
+  }
+  if (this->preset_modes_siid_ != 0 && this->preset_modes_piid_ != 0) {
+    this->parent_->register_listener(this->preset_modes_siid_, this->preset_modes_piid_, true, mvtUInt, [this](const MiotValue &value) {
+      ESP_LOGV(TAG, "MCU reported preset mode %" PRIu32 ":%" PRIu32 " is: %u", this->preset_modes_siid_, this->preset_modes_piid_, value.as_uint);
+      auto it = preset_modes_.find(value.as_uint);
+      if (it == preset_modes_.end()) {
+        ESP_LOGW(TAG, "Invalid value %" PRIu32 "", value.as_uint);
+        return;
+      }
+
+      this->preset_mode = it->second;
+      this->publish_state();
+    });
+  }
+}
+
+void MiotFan::dump_config() {
+  LOG_FAN("", "MIoT Fan", this);
+  ESP_LOGCONFIG(TAG, "  State SIID: %" PRIu32, this->state_siid_);
+  ESP_LOGCONFIG(TAG, "  State PIID: %" PRIu32, this->state_piid_);
+  ESP_LOGCONFIG(TAG, "  Speed SIID: %" PRIu32, this->speed_siid_);
+  ESP_LOGCONFIG(TAG, "  Speed PIID: %" PRIu32, this->speed_piid_);
+  if (this->oscillating_siid_ != 0 && this->oscillating_piid_ != 0) {
+    ESP_LOGCONFIG(TAG, "  Oscillating SIID: %" PRIu32, this->oscillating_siid_);
+    ESP_LOGCONFIG(TAG, "  Oscillating PIID: %" PRIu32, this->oscillating_piid_);
+  }
+  if (this->direction_siid_ != 0 && this->direction_piid_ != 0) {
+    ESP_LOGCONFIG(TAG, "  Direction SIID: %" PRIu32, this->direction_siid_);
+    ESP_LOGCONFIG(TAG, "  Direction PIID: %" PRIu32, this->direction_piid_);
+  }
+  if (this->preset_modes_siid_ != 0 && this->preset_modes_piid_ != 0) {
+    ESP_LOGCONFIG(TAG, "  Preset Modes SIID: %" PRIu32, this->preset_modes_siid_);
+    ESP_LOGCONFIG(TAG, "  Preset Modes PIID: %" PRIu32, this->preset_modes_piid_);
+    ESP_LOGCONFIG(TAG, "  Preset Modes are:");
+    for (auto const &it : this->preset_modes_)
+      ESP_LOGCONFIG(TAG, "    %u: %s", it.first, it.second.c_str());
+    }
+}
+
+fan::FanTraits MiotFan::get_traits() {
+  fan::FanTraits traits(this->oscillating_siid_ != 0 && this->oscillating_piid_ != 0,
+                        this->speed_siid_ != 0 && this->speed_piid_ != 0,
+                        this->direction_siid_ != 0 && this->direction_piid_ != 0,
+                        (this->speed_max_ - this->speed_min_) / this->speed_step_ + 1);
+
+  std::set<std::string> preset_modes;
+  for (auto &iter : preset_modes_)
+    preset_modes.insert(iter.second);
+  traits.set_supported_preset_modes(preset_modes);
+
+  return traits;
+}
+
+void MiotFan::control(const fan::FanCall &call) {
+  if (call.get_state().has_value())
+    this->parent_->set_property(this->state_siid_, this->state_piid_, MiotValue(*call.get_state() ? "true" : "false"));
+  if (call.get_speed().has_value())
+    this->parent_->set_property(this->speed_siid_, this->speed_piid_, MiotValue(this->speed_min_ + *call.get_speed() * this->speed_step_ - 1));
+  if (this->oscillating_siid_ != 0 && this->oscillating_piid_ != 0 && call.get_oscillating().has_value())
+    this->parent_->set_property(this->oscillating_siid_, this->oscillating_piid_, MiotValue(*call.get_oscillating() ? "true" : "false"));
+  if (this->direction_siid_ != 0 && this->direction_piid_ != 0 && call.get_direction().has_value())
+    this->parent_->set_property(this->direction_siid_, this->direction_piid_, MiotValue(*call.get_direction() == fan::FanDirection::REVERSE ? "true" : "false"));
+  if (this->preset_modes_siid_ != 0 && this->preset_modes_piid_ != 0) {
+    const std::string &mode = call.get_preset_mode();
+    auto it = std::find_if(preset_modes_.begin(),
+                           preset_modes_.end(),
+                           [mode](auto && pair) {
+                              return pair.second == mode;
+                           });
+    if (it != preset_modes_.end())
+      this->parent_->set_property(this->preset_modes_siid_, this->preset_modes_piid_, MiotValue(it->first));
+  }
+}
+
+}  // namespace miot
+}  // namespace esphome

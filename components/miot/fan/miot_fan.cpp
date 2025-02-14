29c3cc39bee3e86e67e9133dd8805cfa1be733b7
Andre Heider <a.heider@gmail.com>
2025-02-14 08:41:35 +0100

    f cleanup

--- components/miot/fan/miot_fan.cpp
+++ components/miot/fan/miot_fan.cpp
@@ -17,38 +17,38 @@ void MiotFan::setup() {
     ESP_LOGV(TAG, "MCU reported state %" PRIu32 ":%" PRIu32 " is: %s", this->state_siid_, this->state_piid_, ONOFF(this->state));
     this->publish_state();
   });
+
   this->parent_->register_listener(this->speed_siid_, this->speed_piid_, true, mvtUInt, [this](const MiotValue &value) {
     ESP_LOGV(TAG, "MCU reported speed %" PRIu32 ":%" PRIu32 " is: %u", this->speed_siid_, this->speed_piid_, value.as_uint);
     this->speed = value.as_uint / this->speed_step_ - this->speed_min_ + 1;
     this->publish_state();
   });
-  if (this->oscillating_siid_ != 0 && this->oscillating_piid_ != 0) {
+
+  if (this->oscillating_siid_ != 0 && this->oscillating_piid_ != 0)
     this->parent_->register_listener(this->oscillating_siid_, this->oscillating_piid_, true, mvtString, [this](const MiotValue &value) {
       this->oscillating = value.as_string == "true";
       ESP_LOGV(TAG, "MCU reported oscillating %" PRIu32 ":%" PRIu32 " is: %s", this->oscillating_siid_, this->oscillating_piid_, ONOFF(this->oscillating));
       this->publish_state();
     });
-  }
-  if (this->direction_siid_ != 0 && this->direction_piid_ != 0) {
+
+  if (this->direction_siid_ != 0 && this->direction_piid_ != 0)
     this->parent_->register_listener(this->direction_siid_, this->direction_piid_, true, mvtString, [this](const MiotValue &value) {
       this->direction = value.as_string == "true" ? fan::FanDirection::REVERSE : fan::FanDirection::FORWARD;
       ESP_LOGV(TAG, "MCU reported reverse direction %" PRIu32 ":%" PRIu32 " is: %s", this->direction_siid_, this->direction_piid_, ONOFF(this->direction));
       this->publish_state();
     });
-  }
-  if (this->preset_modes_siid_ != 0 && this->preset_modes_piid_ != 0) {
+
+  if (this->preset_modes_siid_ != 0 && this->preset_modes_piid_ != 0)
     this->parent_->register_listener(this->preset_modes_siid_, this->preset_modes_piid_, true, mvtUInt, [this](const MiotValue &value) {
       ESP_LOGV(TAG, "MCU reported preset mode %" PRIu32 ":%" PRIu32 " is: %u", this->preset_modes_siid_, this->preset_modes_piid_, value.as_uint);
       auto it = preset_modes_.find(value.as_uint);
       if (it == preset_modes_.end()) {
-        ESP_LOGW(TAG, "Invalid value %" PRIu32 "", value.as_uint);
+        ESP_LOGE(TAG, "Unknown preset mode value %" PRIu32 "", value.as_uint);
         return;
       }
-
       this->preset_mode = it->second;
       this->publish_state();
     });
-  }
 }
 
 void MiotFan::dump_config() {
@@ -71,7 +71,7 @@ void MiotFan::dump_config() {
     ESP_LOGCONFIG(TAG, "  Preset Modes are:");
     for (auto const &it : this->preset_modes_)
       ESP_LOGCONFIG(TAG, "    %u: %s", it.first, it.second.c_str());
-    }
+  }
 }
 
 fan::FanTraits MiotFan::get_traits() {
@@ -81,7 +81,7 @@ fan::FanTraits MiotFan::get_traits() {
                         (this->speed_max_ - this->speed_min_) / this->speed_step_ + 1);
 
   std::set<std::string> preset_modes;
-  for (auto &iter : preset_modes_)
+  for (auto const &iter : preset_modes_)
     preset_modes.insert(iter.second);
   traits.set_supported_preset_modes(preset_modes);
 
@@ -106,6 +106,8 @@ void MiotFan::control(const fan::FanCall &call) {
                            });
     if (it != preset_modes_.end())
       this->parent_->set_property(this->preset_modes_siid_, this->preset_modes_piid_, MiotValue(it->first));
+    else
+      ESP_LOGE(TAG, "Unknown preset mode '%s'", mode.c_str());
   }
 }
 

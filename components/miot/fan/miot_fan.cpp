7027eb6f88833daf4d64114ccb3771e608aa21e5
Andre Heider <a.heider@gmail.com>
2025-06-16 06:44:05 +0200

    require esphome>=2025.6.0 for the fan component
    https://github.com/esphome/esphome/pull/8277 has been merged. Requiring
    it allows us to get rid of the hack which works around it.

--- components/miot/fan/miot_fan.cpp
+++ components/miot/fan/miot_fan.cpp
@@ -1,5 +1,10 @@
 #include "esphome/core/log.h"
+#include "esphome/core/version.h"
 #include "miot_fan.h"
+
+#if ESPHOME_VERSION_CODE < VERSION_CODE(2025, 6, 0)
+#error "The fan component requires at least ESPHome 2025.6.0 due to https://github.com/esphome/esphome/pull/8277"
+#endif
 
 namespace esphome {
 namespace miot {
